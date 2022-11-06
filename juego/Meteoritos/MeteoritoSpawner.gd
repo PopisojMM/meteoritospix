4165bfd42ee7f1029e411b908b3f9b7295dce18f
PopisojMM <66846834+PopisojMM@users.noreply.github.com>
2022-11-06 19:41:15 -0300

    Pulido
    Puliendo de a poco

--- juego/Meteoritos/MeteoritoSpawner.gd
+++ juego/Meteoritos/MeteoritoSpawner.gd
@@ -6,11 +6,7 @@ export var direccion:Vector2 = Vector2(1, 1)
 export var rango_tamanio_meteorito:Vector2 = Vector2(0.5, 2.2)
 
 
-## Metodos
-#func _ready() -> void:
-#	yield(owner, "ready")
-#	spawnear_meteorito()
-	
+
 func spawnear_meteorito() -> void:
 	Eventos.emit_signal(
 		"spawn_meteorito",
