0d22622d48b6cf8839fc07d0a4cc1c752bb000c5
PopisojMM <66846834+PopisojMM@users.noreply.github.com>
2022-11-06 12:45:35 -0300

    SEM 5 - MINIMAPA
    ZOOM sigue deformando, al destruir enemigos se reinicia el nivel - REVISAR conexiones -

--- juego/UI/ContenedorInformacion.gd
+++ juego/UI/ContenedorInformacion.gd
@@ -2,8 +2,17 @@
 class_name ContenedorInformacion
 extends NinePatchRect
 
+var esta_activo:bool = true setget set_esta_activo
+
 ## Atributos Export
-export var auto_ocultar:bool = false
+export var auto_ocultar:bool = false setget set_auto_ocultar
+
+## Getter & Setters
+func set_auto_ocultar(valor: bool) -> void:
+	auto_ocultar = valor
+
+func set_esta_activo(valor: bool) -> void:
+	esta_activo = valor
 
 ## Atributos Onready
 onready var texto_contenedor:Label = $Label
@@ -12,18 +21,24 @@ onready var animaciones:AnimationPlayer = $AnimationPlayer
 
 ## Metodos
 func mostrar() -> void:
-	$AnimationPlayer.play("mostrar")
+	if esta_activo:
+		$AnimationPlayer.play("mostrar")
 
 func ocultar() -> void:
+	if not esta_activo:
+		animaciones.stop()
 	$AnimationPlayer.play("ocultar")
 
 func mostrar_suavizado() -> void:
+	if not esta_activo:
+		return
 	$AnimationPlayer.play("mostrar_suavizado")
 	if auto_ocultar:
 		auto_ocultar_timer.start()
 
 func ocultar_suavizado() -> void:
-	$AnimationPlayer.play("ocultar_suavizado")
+	if esta_activo:
+		$AnimationPlayer.play("ocultar_suavizado")
 
 func modificar_texto(text: String) -> void:
 	texto_contenedor.text = text
--- juego/UI/HUD.gd
+++ juego/UI/HUD.gd
@@ -4,12 +4,9 @@ extends CanvasLayer
 ## Atributos Onready
 onready var info_zona_recarga:ContenedorInformacion = $InfoZonaRecarga
 onready var info_meteoritos:ContenedorInformacion = $InfoMeteoritos
-onready var info_tiempo_restante:ContenedorInformacion = $InfoTiempoRestante setget set_auto_ocultar
-
-## Getter & Setters
-func set_auto_ocultar(valor: bool) -> void:
-	set_auto_ocultar = valor
-
+onready var info_tiempo_restante:ContenedorInformacion = $InfoTiempoRestante
+onready var info_laser:ContenedorInformacionEnergia = $EnergiaLaser
+onready var info_escudo:ContenedorInformacionEnergia = $EnergiaEscudo
 
 ## Metodos
 func _ready() -> void:
@@ -22,6 +19,10 @@ func conectar_seniales() -> void:
 	Eventos.connect("detecto_zona_recarga", self, "_on_detecto_zona_recarga")
 	Eventos.connect("cambio_numero_meteoritos", self, "_on_actualizar_info_meteoritos")
 	Eventos.connect("actualizar_tiempo", self, "_on_actualizar_info_tiempo")
+	Eventos.connect("cambio_energia_laser", self, "_on_actualizar_energia_laser")
+	Eventos.connect("ocultar_energia_laser", info_laser, "ocultar")
+	Eventos.connect("cambio_energia_escudo", self, "_on_actualizar_energia_escudo")
+	Eventos.connect("ocultar_energia_escudo", info_escudo, "ocultar")
 
 func fade_in() -> void:
 	$FadeCanvas/AnimationPlayer.play("fade_in")
@@ -49,7 +50,20 @@ func _on_actualizar_info_tiempo(tiempo_restante: int) -> void:
 	)
 	if tiempo_restante % 10 == 0:
 		info_tiempo_restante.mostrar_suavizado()
-#	if tiempo_restante == 0:
-#		info_tiempo_restante
+	if tiempo_restante == 11:
+		info_tiempo_restante.set_auto_ocultar(false)
 	elif tiempo_restante == 0:
 		info_tiempo_restante.ocultar()
+
+func _on_actualizar_energia_laser(energia_max: float, energia_actual: float) -> void:
+	info_laser.mostrar()
+	info_laser.actualizar_energia(energia_max, energia_actual)
+
+func _on_actualizar_energia_escudo(energia_max: float, energia_actual: float) -> void:
+	info_escudo.mostrar()
+	info_escudo.actualizar_energia(energia_max, energia_actual)
+
+func _on_nave_destruida(nave: NaveBase, _posicion) -> void:
+	if nave is Player:
+		get_tree().call_group("contenedor_info", "set_esta_activo", false)
+		get_tree().call_group("contenedor_info",  "ocultar")
